3deac76ad9bf4a88a0772926afe06f361ddf7f52
= <=>
2024-09-24 22:42:00 +0200

    Y yo que coño se

---
+++ src/types/Hero.d.ts
@@ -0,0 +1,2 @@
+type Dota2Role = 'Carry' | 'Support' | 'Nuker' | 'Disabler' | 'Jungler' | 'Durable' | 'Escape' | 'Pusher' | 'Initiator'
+type Dota2Atribute = 'Strength' | 'Agility' | 'Intelligence' | 'Universal'
